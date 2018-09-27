103be0fcbc2f50c729a3a7de5b4343ecb23efd44
Konstantin Zagulin <konstzv@gmail.com>
2018-09-27 11:41:57 +0300

    Added bottom sheet

--- app/src/main/java/com/zagulin/mycard/MainActivity.kt
+++ app/src/main/java/com/zagulin/mycard/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
+import android.support.design.widget.BottomSheetBehavior.BottomSheetCallback
 import android.support.v7.app.AppCompatActivity
 import android.view.Gravity
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 import kotlinx.android.synthetic.main.main_block.*
 
 
@@ -18,17 +22,37 @@ class MainActivity : AppCompatActivity() {
         private const val copyringPaddingDp = 50F
     }
 
+    var bottomSheetBehavior: BottomSheetBehavior<View>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setBottomSheet()
         addCopyring()
         setListeners()
-        setToolbarIfPortrait()
+        setCustomToolbarIfPortrait()
 
     }
 
-    private fun setToolbarIfPortrait() {
+    private fun setBottomSheet() {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+        bottomSheetBehavior!!.state = BottomSheetBehavior.STATE_HIDDEN
+        bottomSheetBehavior!!.setBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    fab.show()
+                } else {
+                    fab.hide()
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+        })
+    }
+
+    private fun setCustomToolbarIfPortrait() {
         val orientation = resources.configuration.orientation
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             setSupportActionBar(toolbar)
@@ -47,41 +71,74 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        send_msg_btn.setOnClickListener { sendMessageFromUser() }
+
+        send_msg_btn.setOnClickListener {
+            onSendBtnClick()
+        }
         linkedin_link.setOnClickListener { openUrl(getString(R.string.linkedin_url)) }
         github_link.setOnClickListener { openUrl(getString(R.string.github_url)) }
         telegram_link.setOnClickListener { openUrl(getString(R.string.telegram_url)) }
+        cancel_btn.setOnClickListener { bottomSheetBehavior!!.state = BottomSheetBehavior.STATE_HIDDEN }
+        fab.setOnClickListener { onFabCLick() }
+
+        edit_text.setOnEditorActionListener { _, _, _ ->
+            onSendBtnClick()
+            return@setOnEditorActionListener true
+        }
+    }
+
+    private fun onSendBtnClick() {
+        if (sendMessageFromUser()) {
+            bottomSheetBehavior!!.state = BottomSheetBehavior.STATE_HIDDEN
+            edit_text.setText("")
+        }
+    }
+
+    private fun onFabCLick() {
+
+        if (bottomSheetBehavior != null) {
+            if (bottomSheetBehavior!!.state == BottomSheetBehavior.STATE_HIDDEN) {
+                bottomSheetBehavior!!.state = BottomSheetBehavior.STATE_EXPANDED
+
+            } else {
+                bottomSheetBehavior!!.state = BottomSheetBehavior.STATE_HIDDEN
+
+            }
+        }
     }
 
     private fun openUrl(url: String) {
         val openUrlIntent = Intent(Intent.ACTION_VIEW)
         openUrlIntent.data = Uri.parse(url)
         if (checkIntentResolving(openUrlIntent)) {
-            startActivity(intent)
+            startActivity(openUrlIntent)
         } else {
             Toast.makeText(this, R.string.no_view_url_client_error, Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun sendMessageFromUser() {
+    private fun sendMessageFromUser(): Boolean {
         val message = edit_text.text.toString()
         if (message.isBlank()) {
             Toast.makeText(this, R.string.edit_text_hint, Toast.LENGTH_LONG).show()
-            return
+            return false
         }
-        sendMessageByEmail(message)
+        return sendMessageByEmail(message)
+
     }
 
-    private fun sendMessageByEmail(message: String) {
+    private fun sendMessageByEmail(message: String): Boolean {
         val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
             data = Uri.parse(MAIL_TO_URI)
             putExtra(Intent.EXTRA_SUBJECT, getString(R.string.default_mail_subject))
             putExtra(Intent.EXTRA_TEXT, message)
         }
-        if (checkIntentResolving(emailIntent)) {
-            startActivity(intent)
+        return if (checkIntentResolving(emailIntent)) {
+            startActivity(emailIntent)
+            true
         } else {
             Toast.makeText(this, R.string.no_email_client_error, Toast.LENGTH_LONG).show()
+            false
         }
     }
 
