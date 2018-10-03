7052648a7a12ed92fa1017e26dde332bd627d707
Konstantin Zagulin <konstzv@gmail.com>
2018-10-03 16:20:04 +0300

    Toolbar for feed screen Labels for activities

--- app/src/main/java/com/zagulin/mycard/FeedActivity.kt
+++ app/src/main/java/com/zagulin/mycard/FeedActivity.kt
@@ -1,6 +1,9 @@
 package com.zagulin.mycard
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class FeedActivity : AppCompatActivity() {
@@ -8,6 +11,23 @@ class FeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.feed_activity)
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.feed_screen_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when (item?.itemId) {
+
+        R.id.feedAboutMenu -> {
+            startActivity(Intent(this, AboutActivity::class.java))
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+
+    }
+
 
 }
