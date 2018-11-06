4334ff8cead3578d1c29cfce3d6027448d2ccddf
Konstantin Zagulin <konstzv@gmail.com>
2018-11-06 10:20:08 +0300

    added rx error handler

--- app/src/main/java/com/zagulin/mycard/App.kt
+++ app/src/main/java/com/zagulin/mycard/App.kt
@@ -1,13 +1,23 @@
 package com.zagulin.mycard
 
 import android.app.Application
+import android.util.Log
 import net.danlew.android.joda.JodaTimeAndroid
+
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
+
+
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         JodaTimeAndroid.init(this)
+        RxJavaPlugins.setErrorHandler { e ->
+
+            Log.w("RX","Undeliverable exception received, not sure what to do", e)
+        }
     }
 
 }
